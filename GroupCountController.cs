958d168a0c9d7efd95f67f80271d5e83d683ae2a
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-03-01 19:27:05 +0800

    Provide free dining tables and plates for large group sizes

--- GroupCountController.cs
+++ GroupCountController.cs
@@ -1,6 +1,6 @@
 ﻿using Kitchen;
+using KitchenCustomDifficulty.Patches;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 using Unity.Entities;
 
@@ -10,6 +10,11 @@ namespace KitchenCustomDifficulty
     [UpdateInGroup(typeof(TimeManagementGroup))]
     internal class GroupCountController : RestaurantSystem
     {
+        private MethodInfo m_MaxTableSize = typeof(CreateCustomerSchedule).GetMethod("MaxTableSize", BindingFlags.NonPublic | BindingFlags.Instance);
+        private MethodInfo m_TotalPlates = typeof(CreateCustomerSchedule).GetMethod("TotalPlates", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private GrantNecessaryAppliances grantNecessaryAppliancesInstance;
+
         private struct CustomerSettings
         {
             public int Enabled;
@@ -43,9 +48,29 @@ namespace KitchenCustomDifficulty
 
         protected override void OnUpdate()
         {
+            if (CreateCustomerSchedule_Patch.Performed)
+            {
+                grantNecessaryAppliancesInstance = World.GetExistingSystem<GrantNecessaryAppliances>();
+
+                if (grantNecessaryAppliancesInstance != null)
+                {
+                    SKitchenParameters kitchenParameters = GetOrDefault<SKitchenParameters>();
+                    int maxGroupSize = kitchenParameters.Parameters.MaximumGroupSize;
+                    if (maxGroupSize < (int)m_MaxTableSize.Invoke(grantNecessaryAppliancesInstance, null) &&
+                        maxGroupSize < (int)m_TotalPlates.Invoke(grantNecessaryAppliancesInstance, null))
+                    {
+                        GroupSizeController.ResetKitchenParameters();
+                        CreateCustomerSchedule_Patch.Performed = false;
+                    }
+                }
+                return;
+            }
+
+
             if (!Has<SIsNightTime>())
             {
                 prevSettings = default;
+                return;
             }
 
             CustomerSettings customerSettings = new CustomerSettings
--- Patches/CreateCustomerSchedule_Patch.cs
+++ Patches/CreateCustomerSchedule_Patch.cs
@@ -1,28 +1,19 @@
 ﻿using HarmonyLib;
 using Kitchen;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace KitchenCustomDifficulty.Patches
 {
     [HarmonyPatch]
     internal class CreateCustomerSchedule_Patch
     {
+        public static bool Performed = false;
+
         [HarmonyPatch(typeof(CreateCustomerSchedule), "OnUpdate")]
         [HarmonyPrefix]
         static void OnUpdate_Prefix()
         {
             GroupSizeController.UseCustomKitchenParameters();
-        }
-
-        [HarmonyPatch(typeof(CreateCustomerSchedule), "OnUpdate")]
-        [HarmonyPostfix]
-        static void OnUpdate_Postfix()
-        {
-            GroupSizeController.ResetKitchenParameters();
+            Performed = true;
         }
     }
 }
