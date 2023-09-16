b1ddcc1dd4c354ad1e9cb0f4097761d1d9b6c721
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-09-16 08:21:40 +0800

    Added Cheats Handler

---
+++ HandleCheats.cs
@@ -0,0 +1,50 @@
+﻿using Kitchen;
+using KitchenMods;
+
+namespace KitchenCustomDifficulty
+{
+    public class HandleCheats : RestaurantSystem, IModSystem
+    {
+        protected override void Initialise()
+        {
+            base.Initialise();
+        }
+
+        protected override void OnUpdate()
+        {
+            switch (Main.PrefSysManager.Get<int>(Main.CHEAT_NO_LOSING_ID))
+            {
+                case 0:
+                    if (Has<SCheatNoLosing>())
+                        Clear<SCheatNoLosing>();
+                    break;
+                case 1:
+                    if (!Has<SCheatNoLosing>())
+                        Set<SCheatNoLosing>();
+                    break;
+            }
+            int instantProcesses = Main.PrefSysManager.Get<int>(Main.CHEAT_INSTANT_PROCESSES_ID);
+            if (instantProcesses < 1)
+            {
+                if (Has<SCheatInstantProcesses>())
+                    Clear<SCheatInstantProcesses>();
+                if (Has<SCheatNoBadProcesses>())
+                    Clear<SCheatNoBadProcesses>();
+            }
+            else if (instantProcesses < 2)
+            {
+                if (!Has<SCheatInstantProcesses>())
+                    Set<SCheatInstantProcesses>();
+                if (!Has<SCheatNoBadProcesses>())
+                    Set<SCheatNoBadProcesses>();
+            }
+            else
+            {
+                if (!Has<SCheatInstantProcesses>())
+                    Set<SCheatInstantProcesses>();
+                if (Has<SCheatNoBadProcesses>())
+                    Clear<SCheatNoBadProcesses>();
+            }
+        }
+    }
+}
