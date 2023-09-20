10c20204cbee83a87b2345576fe4f45ae7bd894b
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-09-20 21:53:32 +0800

    Refactored custom restart chance system

---
+++ Patches/CheckGameOverFromLife_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Kitchen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenCustomDifficulty.Patches
+{
+    [HarmonyPatch]
+    static class CheckGameOverFromLife_Patch
+    {
+
+        [HarmonyPatch(typeof(CheckGameOverFromLife), "RescuedByDay")]
+        [HarmonyPrefix]
+        static bool RescuedByDay_Prefix(ref bool __result)
+        {
+            __result = PatchController.CustomOfferRestart(out bool shouldRunOriginal);
+            return shouldRunOriginal;
+        }
+    }
+}
